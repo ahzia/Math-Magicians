f916bbc9ea5bc5ae7c45f4d4c4bcdd3a621d3c46
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-22 21:10:46 +0430

    Basic Layout for calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+export default class Calculator extends Component{
+    render(){
+        return(
+            <div className='container'>
+                <div className='calculator bg-second d-flex flex-column'>
+                    <div className='bg-first viewer'> </div>
+
+                </div>
+                
+            </div>
+        );
+    }
+}
