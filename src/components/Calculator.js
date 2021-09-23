eeb641273e3610a83e292bf065f83e9042ca9308
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-23 14:59:25 +0430

    make design same as wireframe

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,34 +7,34 @@ export default class Calculator extends Component {
       <div className="container">
         <div className="calculator bg-second d-flex flex-column">
           <div className="bg-first viewer"> </div>
-          <div className="d-flex m-1">
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">AC</div>
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">+/-</div>
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">%</div>
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">/</div>
+          <div className="d-flex">
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">AC</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">+/-</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">%</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border bg-temp">/</div>
           </div>
-          <div className="d-flex m-1">
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">7</div>
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">8</div>
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">9</div>
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">X</div>
+          <div className="d-flex">
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">7</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">8</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">9</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border bg-temp">X</div>
           </div>
-          <div className="d-flex m-1">
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">4</div>
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">5</div>
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">6</div>
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">-</div>
+          <div className="d-flex">
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">4</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">5</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">6</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border bg-temp">-</div>
           </div>
-          <div className="d-flex m-1">
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">1</div>
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">2</div>
-            <div className="btn-rectingle text-first d-flex justify-content-center align-items-center">3</div>
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">+</div>
+          <div className="d-flex">
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">1</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">2</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">3</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border bg-temp">+</div>
           </div>
-          <div className="d-flex m-1">
-            <div className="btn-rectingle btn-zero text-first d-flex justify-content-center align-items-center">0</div>
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">.</div>
-            <div className="btn-rectingle text-white d-flex justify-content-center align-items-center">=</div>
+          <div className="d-flex">
+            <div className="btn-rectingle btn-zero d-flex justify-content-center align-items-center border">0</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border">.</div>
+            <div className="btn-rectingle d-flex justify-content-center align-items-center border bg-temp">=</div>
           </div>
         </div>
       </div>
