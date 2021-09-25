ceb02a688022c5eec2503f6c653831f35c2fdafb
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-25 09:55:59 +0430

    fix linters issue

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,53 +10,53 @@ const Calculator = () => {
   });
 
   const handleClick = (buttonName) => {
-    const newObject= calculate(caculatorObject, buttonName);
+    const newObject = calculate(caculatorObject, buttonName);
     setCalculatorObject(newObject);
-  }
+  };
 
-  return(
+  return (
     <div className="container">
-    <div className="calculator bg-second d-flex flex-column">
-      <div className="bg-first viewer d-flex">
-        <div className="col-2 p-3">
-          <p>{caculatorObject.operation}</p>
+      <div className="calculator bg-second d-flex flex-column">
+        <div className="bg-first viewer d-flex">
+          <div className="col-2 p-3">
+            <p>{caculatorObject.operation}</p>
+          </div>
+          <div className="col-10 d-flex flex-column align-items-end justify-content-between p-3">
+            <p>{caculatorObject.total}</p>
+            <p>{caculatorObject.next}</p>
+          </div>
         </div>
-        <div className="col-10 d-flex flex-column align-items-end justify-content-between p-3">
-          <p>{caculatorObject.total}</p>
-          <p>{caculatorObject.next}</p>
+        <div className="d-flex">
+          <CalculatorButton buttonName="AC" handleClick={handleClick} />
+          <CalculatorButton buttonName="+/-" handleClick={handleClick} />
+          <CalculatorButton buttonName="%" handleClick={handleClick} />
+          <CalculatorButton buttonName="÷" handleClick={handleClick} className="bg-temp" />
         </div>
-      </div>
-      <div className="d-flex">
-        <CalculatorButton buttonName="AC" handleClick={handleClick} />
-        <CalculatorButton buttonName="+/-" handleClick={handleClick} />
-        <CalculatorButton buttonName="%" handleClick={handleClick} />
-        <CalculatorButton buttonName="÷" handleClick={handleClick} className="bg-temp" />
-      </div>
-      <div className="d-flex">
-        <CalculatorButton buttonName="7" handleClick={handleClick} />
-        <CalculatorButton buttonName="8" handleClick={handleClick} />
-        <CalculatorButton buttonName="9" handleClick={handleClick} />
-        <CalculatorButton buttonName="x" handleClick={handleClick} className="bg-temp" />
-      </div>
-      <div className="d-flex">
-        <CalculatorButton buttonName="4" handleClick={handleClick} />
-        <CalculatorButton buttonName="5" handleClick={handleClick} />
-        <CalculatorButton buttonName="6" handleClick={handleClick} />
-        <CalculatorButton buttonName="-" handleClick={handleClick} className="bg-temp" />
-      </div>
-      <div className="d-flex">
-        <CalculatorButton buttonName="1" handleClick={handleClick} />
-        <CalculatorButton buttonName="2" handleClick={handleClick} />
-        <CalculatorButton buttonName="3" handleClick={handleClick} />
-        <CalculatorButton buttonName="+" handleClick={handleClick} className="bg-temp" />
-      </div>
-      <div className="d-flex">
-        <CalculatorButton buttonName="0" handleClick={handleClick} className="btn-zero" />
-        <CalculatorButton buttonName="." handleClick={handleClick} />
-        <CalculatorButton buttonName="=" handleClick={handleClick} className="bg-temp" />
+        <div className="d-flex">
+          <CalculatorButton buttonName="7" handleClick={handleClick} />
+          <CalculatorButton buttonName="8" handleClick={handleClick} />
+          <CalculatorButton buttonName="9" handleClick={handleClick} />
+          <CalculatorButton buttonName="x" handleClick={handleClick} className="bg-temp" />
+        </div>
+        <div className="d-flex">
+          <CalculatorButton buttonName="4" handleClick={handleClick} />
+          <CalculatorButton buttonName="5" handleClick={handleClick} />
+          <CalculatorButton buttonName="6" handleClick={handleClick} />
+          <CalculatorButton buttonName="-" handleClick={handleClick} className="bg-temp" />
+        </div>
+        <div className="d-flex">
+          <CalculatorButton buttonName="1" handleClick={handleClick} />
+          <CalculatorButton buttonName="2" handleClick={handleClick} />
+          <CalculatorButton buttonName="3" handleClick={handleClick} />
+          <CalculatorButton buttonName="+" handleClick={handleClick} className="bg-temp" />
+        </div>
+        <div className="d-flex">
+          <CalculatorButton buttonName="0" handleClick={handleClick} className="btn-zero" />
+          <CalculatorButton buttonName="." handleClick={handleClick} />
+          <CalculatorButton buttonName="=" handleClick={handleClick} className="bg-temp" />
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
--- src/components/CalcultorButton.js
+++ src/components/CalcultorButton.js
@@ -1,17 +1,14 @@
 const CalculatorButton = (props) => {
-   
-    const handleClick = (e) => {
-      const buttonName = e.target.id;
-      props.handleClick(buttonName);
-    }
-  
-    const className = `btn-rectingle d-flex justify-content-center align-items-center border ${props.className}`;
-  
-    return (
-      <div id={props.buttonName} onClick={handleClick} onKeyDown={handleClick} role="button" className={className} tabIndex={0}>
-        {props.buttonName}
-      </div>
-    );
+  const handleClick = (e) => {
+    const buttonName = e.target.id;
+    props.handleClick(buttonName);
   }
-  
-  export default CalculatorButton;
+  const className = `btn-rectingle d-flex justify-content-center align-items-center border ${props.className}`;
+  return (
+    <div id={props.buttonName} onClick={handleClick} onKeyDown={handleClick} role="button" className={className} tabIndex={0}>
+      {props.buttonName}
+    </div>
+  );
+}
+
+export default CalculatorButton;
