29fae0c8e8ffb783c85b9b85a0b011bc31379f41
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-10-01 23:24:13 +0430

    fix linter issue

--- src/Pages/CalculatorPage.js
+++ src/Pages/CalculatorPage.js
@@ -1,7 +1,7 @@
 import Calculator from "../components/Calculator";
 const CalculatorPage = () => {
   return(
-    <section class='d-flex my-5 align-items-center'>
+    <section className='d-flex my-5 align-items-center'>
       <h3 className='text-first'>Let's do Some Math!</h3>
       <Calculator />
     </section>
