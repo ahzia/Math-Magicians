8a7d95a055c697df94633732ad0285a3c0947bb8
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-10-02 09:52:50 +0430

    fix linters Issue

--- src/components/CalculatorScreen.js
+++ src/components/CalculatorScreen.js
@@ -1,26 +1,25 @@
 const CalculatorScreen = (props) => {
-  if(props.caculatorObject.total===null && props.caculatorObject.next === null){
-    return(
+  if (props.caculatorObject.total === null && props.caculatorObject.next === null) {
+    return (
       <div className="bg-first viewer d-flex">
         <div className="col-12 d-flex flex-column align-items-end justify-content-center p-3">
           <p>0</p>
         </div>
       </div>
     );
-  }
-  else{
-    return(
+  } else {
+    return (
       <div className="bg-first viewer d-flex">
         <div className="col-2 p-3">
           <p>{props.caculatorObject.operation}</p>
         </div>
-        <div className="col-10 d-flex flex-column align-items-end justify-content-between p-3">  
+        <div className="col-10 d-flex flex-column align-items-end justify-content-between p-3">
           <p>{props.caculatorObject.total}</p>
           <p>{props.caculatorObject.next}</p>
         </div>
       </div>
     );
   }
-}
+};
 
 export default CalculatorScreen;
